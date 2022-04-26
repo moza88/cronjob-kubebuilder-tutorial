cb41fd68eb477f42046184dc134d262da3f7cc77
Mabel Oza <ozamabel@gmail.com>
2022-04-25 23:53:18 -0500

    modified cronjob webhook api

--- api/v1/cronjob_webhook.go
+++ api/v1/cronjob_webhook.go
@@ -17,11 +17,18 @@ limitations under the License.
 package v1
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
+    "github.com/robfig/cron"
+    apierrors "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/runtime"
+    "k8s.io/apimachinery/pkg/runtime/schema"
+    validationutils "k8s.io/apimachinery/pkg/util/validation"
+    "k8s.io/apimachinery/pkg/util/validation/field"
+    ctrl "sigs.k8s.io/controller-runtime"
+    logf "sigs.k8s.io/controller-runtime/pkg/log"
+    "sigs.k8s.io/controller-runtime/pkg/webhook"
 )
+// +kubebuilder:docs-gen:collapse=Go imports
+
 
 // log is for logging in this package.
 var cronjoblog = logf.Log.WithName("cronjob-resource")
@@ -32,18 +39,31 @@ func (r *CronJob) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 //+kubebuilder:webhook:path=/mutate-batch-cronjob-kubebuilder-io-v1-cronjob,mutating=true,failurePolicy=fail,sideEffects=None,groups=batch.cronjob.kubebuilder.io,resources=cronjobs,verbs=create;update,versions=v1,name=mcronjob.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &CronJob{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type
 func (r *CronJob) Default() {
-	cronjoblog.Info("default", "name", r.Name)
+    cronjoblog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+    if r.Spec.ConcurrencyPolicy == "" {
+        r.Spec.ConcurrencyPolicy = AllowConcurrent
+    }
+    if r.Spec.Suspend == nil {
+        r.Spec.Suspend = new(bool)
+    }
+    if r.Spec.SuccessfulJobsHistoryLimit == nil {
+        r.Spec.SuccessfulJobsHistoryLimit = new(int32)
+        *r.Spec.SuccessfulJobsHistoryLimit = 3
+    }
+    if r.Spec.FailedJobsHistoryLimit == nil {
+        r.Spec.FailedJobsHistoryLimit = new(int32)
+        *r.Spec.FailedJobsHistoryLimit = 1
+    }
 }
+//+kubebuilder:webhook:verbs=create;update;delete,path=/validate-batch-tutorial-kubebuilder-io-v1-cronjob,mutating=false,failurePolicy=fail,groups=batch.tutorial.kubebuilder.io,resources=cronjobs,versions=v1,name=vcronjob.kb.io,sideEffects=None,admissionReviewVersions=v1
+
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-batch-cronjob-kubebuilder-io-v1-cronjob,mutating=false,failurePolicy=fail,sideEffects=None,groups=batch.cronjob.kubebuilder.io,resources=cronjobs,verbs=create;update,versions=v1,name=vcronjob.kb.io,admissionReviewVersions=v1
@@ -54,16 +74,14 @@ var _ webhook.Validator = &CronJob{}
 func (r *CronJob) ValidateCreate() error {
 	cronjoblog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.validateCronJob()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *CronJob) ValidateUpdate(old runtime.Object) error {
 	cronjoblog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	return r.validateCronJob()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -73,3 +91,54 @@ func (r *CronJob) ValidateDelete() error {
 	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
+
+/*
+	Validate the name and the spec of the CronJob
+ */
+func (r *CronJob) validateCronJob() error {
+
+	var allErrs field.ErrorList
+
+	if err := r.validateCronJobName(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if err := r.validateCronJobSpec(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "batch.cronjob.kubebuilder.io", Kind: "CronJob"},
+		r.Name, allErrs)
+}
+
+func (r *CronJob) validateCronJobSpec() *field.Error {
+	return validateScheduleFormat(
+		r.Spec.Schedule,
+		field.NewPath("spec").Child("schedule"))
+}
+
+//Validate if the schedule is well formatted
+func validateScheduleFormat(schedule string, fldPath *field.Path) *field.Error {
+    if _, err := cron.ParseStandard(schedule); err != nil {
+        return field.Invalid(fldPath, schedule, err.Error())
+    }
+    return nil
+}
+
+//Validating the Cron Job Name, they can only be a max of 52 Characters
+func (r *CronJob) validateCronJobName() *field.Error {
+	if len(r.ObjectMeta.Name) > validationutils.DNS1035LabelMaxLength-11 {
+		// The job name length is 63 character like all Kubernetes objects
+		// (which must fit in a DNS subdomain). The cronjob controller appends
+		// a 11-character suffix to the cronjob (`-$TIMESTAMP`) when creating
+		// a job. The job name length limit is 63 characters. Therefore cronjob
+		// names must have length <= 63-11=52. If we don't validate this here,
+		// then job creation will fail later.
+		return field.Invalid(field.NewPath("metadata").Child("name"), r.Name, "Must be no more than 52 characters")
+	}
+	return nil
+}
+// +kubebuilder:docs-gen:collapse=Validate object name
